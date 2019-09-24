0f1858200d06d4c074a47632343292842fcdac1d
Shiwei Zhou <s1zhou@ucsd.edu>
2019-09-24 15:52:25 -0700

    optimized for async

--- redis-backend/app.js
+++ redis-backend/app.js
@@ -16,24 +16,23 @@ app.get('/redisApi/v0.3/users', async (req, res) => {
   }
 });
 
-app.get('/redisApi/v0.3/sensors', (req, res) => {
-  var sensors = [];
-  redis.scan(0, 'MATCH', 'sensor:info:*', 'COUNT', 10000).then(key_res => {
-    for (let i = 0; i < key_res[1].length; i++) {
-      redis.hgetall(key_res[1][i]).then(user_res => {
-        const newOBJ = Object.assign(user_res);
-        sensors.push(newOBJ);
-        if (i === key_res[1].length - 1) {
-          res.status(200).json({
-            status: 'success',
-            data: {
-              sensors
-            }
-          });
-        }
-      });
-    }
-  });
+app.get('/redisApi/v0.3/sensors', async (req, res) => {
+  try {
+    const key_res = await redis.scan(
+      0,
+      'MATCH',
+      'sensor:info:*',
+      'COUNT',
+      10000
+    );
+    const sensors = await Promise.all(
+      key_res[1].map(key => redis.hgetall(key))
+    );
+
+    res.status(200).json({ status: 'success', data: { sensors } });
+  } catch (error) {
+    res.status(500).json({ status: 'failure', data: { error } });
+  }
 });
 
 const port = 8000;
